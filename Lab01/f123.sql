439be63cf4993049a8c12806e04c3a0552fbf6e9
Lourenco Matias <lourencomatias@unknown0ed461fe28e6.lan>
2026-03-19 01:48:49 +0000

    V1.0_test

---
+++ Lab01/f123.sql
@@ -0,0 +1,7 @@
+-- Feature 1 2 3
+-- RF 1 : Visualizar o histórico de sessões de carregamento de um utilizador num intervalo de tempo -> Query simples
+-- RF 2 : Visualizar todas as sessões realizadas num determinado posto de carregamento -> Query simples
+-- RF 3 : Visualizar todas as sessões de carregamento incluindo nome do utilizador, veículo e localização do posto -> Query com JOIN
+
+-- RF 1
+-- lol
