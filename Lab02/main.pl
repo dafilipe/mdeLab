17b506097e96df345eafa13fdffd9641b5313df1
Lourenco Matias <lourencomatias@10-22-127-124.ed10.eduroam.fct.unl.pt>
2026-04-29 15:26:16 +0100

    menu added

--- Lab02/main.pl
+++ Lab02/main.pl
@@ -1,1 +1,7 @@
 #Ficheiro principal do LAB02
+
+#Robots - Capacity, Type, Autonomy, Speed, Energy Consumption
+#Type: Ground, Drone, Autonomous
+%robot(Type, Autonomy, Speed, EnergyConsumption).
+
+#Nodes - Id, o id vai corresponder a um supplier, hub, charging station ou deliv
